1cc16eb9964a4756858ca772c1ab3a43b032c9b3
MonzerMahmoud <55148623+MonzerMahmoud@users.noreply.github.com>
2022-01-03 05:56:57 +0200

    Initial Commit

---
+++ AudioBookSwiftUI/AudioBookSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioBookSwiftUIApp.swift
+//  AudioBookSwiftUI
+//
+//  Created by Syber Expertise on 03/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct AudioBookSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
